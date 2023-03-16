7dc13e7b3ff666a231ce013d0cfb6be7fb9fb18a
Vedshruti Sarkar <68157517+Vedshruti-11@users.noreply.github.com>
2023-03-16 12:23:20 +0530

    Add files via upload

---
+++ Game.java
@@ -0,0 +1,38 @@
+//TASK2 : NUMBER GUESSING GAME 
+
+//import java.util.*;
+import javax.swing.JOptionPane; //Needed for Dialog Box
+
+public class Game {
+
+	public static void main(String[] args) {
+
+		int guess = 1 + (int) (100 * Math.random());
+
+		int tries = 10;
+		while (tries != 0) {
+			String input = JOptionPane.showInputDialog("Guess the Number between 1 and 100");
+			int number = Integer.parseInt(input);
+			if (number == guess) {
+				int tried = 10 - tries + 1;
+				int score = tries * 10;
+				JOptionPane.showMessageDialog(null, "Congratulations! You have guessed the number correctly in " + tried
+						+ " tries \nYour Accuracy is : " + score + "%");
+
+				break;
+
+			} else if (number > guess) {
+				JOptionPane.showMessageDialog(null, "Incorrect guess! Your input is greater than generated the number");
+			} else {
+				JOptionPane.showMessageDialog(null, "Incorrect guess! Your input is lesser than generated the number");
+			}
+			tries--;
+		}
+		if (tries == 0) {
+			JOptionPane.showMessageDialog(null,
+					"No more trials left! The generated number was " + guess + " \nYour Accuracy is : 0% ");
+		}
+
+	}
+
+}
